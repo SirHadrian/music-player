416ede3e2ad466a4f95eff8ea6d1ae2f91b384bc
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-05-14 09:55:34 +0300

    bindings

--- app/src/main/java/com/sirhadrian/musicplayer/MainActivity.java
+++ app/src/main/java/com/sirhadrian/musicplayer/MainActivity.java
@@ -3,12 +3,18 @@ package com.sirhadrian.musicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.sirhadrian.musicplayer.databinding.FragmentHolderBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        FragmentHolderBinding binding = FragmentHolderBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
---
+++ app/src/main/java/com/sirhadrian/musicplayer/ui/SongsListViewModel.java
@@ -0,0 +1,8 @@
+package com.sirhadrian.musicplayer.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class SongsListViewModel extends ViewModel {
+
+
+}
