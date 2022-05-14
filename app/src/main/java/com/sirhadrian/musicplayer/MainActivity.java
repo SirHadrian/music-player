a850f0a36ecf0265ca6858b5dacd5682a43a3e18
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-05-14 10:42:44 +0300

    Fragment test

--- app/src/main/java/com/sirhadrian/musicplayer/MainActivity.java
+++ app/src/main/java/com/sirhadrian/musicplayer/MainActivity.java
@@ -1,11 +1,14 @@
 package com.sirhadrian.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.sirhadrian.musicplayer.databinding.FragmentHolderBinding;
+import com.sirhadrian.musicplayer.ui.SongsListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +19,15 @@ public class MainActivity extends AppCompatActivity {
         FragmentHolderBinding binding = FragmentHolderBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_holder);
+        if (fragment == null) {
+            fragment=new SongsListFragment();
+            fragmentManager.beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_holder, fragment)
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/sirhadrian/musicplayer/ui/SongsListFragment.java
+++ app/src/main/java/com/sirhadrian/musicplayer/ui/SongsListFragment.java
@@ -9,11 +9,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.sirhadrian.musicplayer.databinding.FragmentSongsListBinding;
+
 public class SongsListFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return null;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        FragmentSongsListBinding binding = FragmentSongsListBinding.inflate(inflater, container,
+                false);
+
+        View view = binding.getRoot();
+
+
+
+        return view;
     }
 }
