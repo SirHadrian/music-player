5e9524d009b7a20eaf29ff92c40d0be493c8ad1b
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-05-15 10:54:21 +0300

    Added toolbar

--- app/src/main/java/com/sirhadrian/musicplayer/ui/SongDetailFragment.java
+++ app/src/main/java/com/sirhadrian/musicplayer/ui/SongDetailFragment.java
@@ -1,6 +1,10 @@
 package com.sirhadrian.musicplayer.ui;
 
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,11 +14,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.sirhadrian.musicplayer.R;
 import com.sirhadrian.musicplayer.databinding.FragmentSongDetailBinding;
+
+import java.io.IOException;
 
 public class SongDetailFragment extends Fragment {
 
     private TextView mSongDetailTitle;
+    private MediaPlayer mediaPlayer;
+    private String[] list;
 
     public SongDetailFragment() {
 
@@ -29,7 +38,15 @@ public class SongDetailFragment extends Fragment {
         View view = binding.getRoot();
         mSongDetailTitle = binding.songDetailTextView;
 
+        mediaPlayer = MediaPlayer.create(getContext(), R.raw.song3);
+        mediaPlayer.start();
 
         return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mediaPlayer.release();
     }
 }
