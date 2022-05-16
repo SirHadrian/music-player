2dcc789d306502d305e7d843ddd9ce9b1839e909
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-05-16 14:47:15 +0300

    scanning storage, loading and playing songs from recyclerview

--- app/src/main/java/com/sirhadrian/musicplayer/ui/SongDetailFragment.java
+++ app/src/main/java/com/sirhadrian/musicplayer/ui/SongDetailFragment.java
@@ -2,6 +2,7 @@ package com.sirhadrian.musicplayer.ui;
 
 import android.media.AudioAttributes;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -13,10 +14,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.sirhadrian.musicplayer.R;
 import com.sirhadrian.musicplayer.databinding.FragmentSongDetailBinding;
 
+import java.io.File;
 import java.io.IOException;
 
 public class SongDetailFragment extends Fragment {
@@ -38,8 +41,32 @@ public class SongDetailFragment extends Fragment {
         View view = binding.getRoot();
         mSongDetailTitle = binding.songDetailTextView;
 
-        mediaPlayer = MediaPlayer.create(getContext(), R.raw.song3);
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_holder);
+
+        assert fragment != null;
+        mSongDetailTitle.setText(fragment.getArguments().getString("name"));
+
+        Uri uri = Uri.fromFile(new File(fragment.getArguments().getString("path")));
+
+        Log.d("uri", fragment.getArguments().getString("path").toString());
+
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioAttributes(
+                new AudioAttributes.Builder()
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .build()
+        );
+        try {
+            mediaPlayer.setDataSource(getContext(), uri);
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         mediaPlayer.start();
+
 
         return view;
     }
--- app/src/main/java/com/sirhadrian/musicplayer/ui/SongsListFragment.java
+++ app/src/main/java/com/sirhadrian/musicplayer/ui/SongsListFragment.java
@@ -1,19 +1,12 @@
 package com.sirhadrian.musicplayer.ui;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.usage.ExternalStorageStats;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -22,8 +15,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -38,11 +29,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SongsListFragment extends Fragment {
-    private ArrayList<HashMap<String, String>> tempSongs;
+    private List<HashMap<String, String>> mSongsList;
 
     private RecyclerView mRecyclerView;
     private SongsAdapter mSongsAdapter;
@@ -68,38 +58,28 @@ public class SongsListFragment extends Fragment {
                             break;
 
                         case R.id.scan:
-                            Log.d("scan", "Am access to external storage");
-
-                            Log.d("scan", "Started Scanning");
                             String myFolderTemp = "/storage/44A6-B704/Documents/Music/E_B_M";
-                            tempSongs = getPlayList(myFolderTemp);
-                            List<String> mySongs = new ArrayList<>();
-                            if (tempSongs != null) {
-                                for (int i = 0; i < tempSongs.size(); i++) {
-                                    String fileName = tempSongs.get(i).get("file_name");
-                                    String filePath = tempSongs.get(i).get("file_path");
-                                    Log.d("scan", " name =" + fileName + " path = " + filePath);
-                                    mySongs.add(fileName);
+                            mSongsList = getPlayList(myFolderTemp);
+
+                            if (mSongsList != null) {
+
+                                mSongsAdapter = new SongsAdapter(mSongsList);
+                                mRecyclerView.setAdapter(mSongsAdapter);
+                            }
+                            /*
+                            Executor executor = Executors.newSingleThreadExecutor();
+                            executor.execute(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Log.d("scan", "Am access to external storage");
+                                    Log.d("scan", "Started Scanning");
+
+
                                 }
-                            }
-
-                            if (mySongs != null) {
-                                mSongsAdapter = new SongsAdapter(mySongs);
-                            }
-                            mRecyclerView.setAdapter(mSongsAdapter);
-                        /*
-                        Executor executor = Executors.newSingleThreadExecutor();
-                        executor.execute(new Runnable() {
-                            @Override
-                            public void run() {
-
-                            }
-                        });*/
-
+                            });
+                            */
 
                             Log.d("scan", "End Scan");
-
-
                             break;
                         default:
                             return false;
@@ -108,34 +88,15 @@ public class SongsListFragment extends Fragment {
                     return true;
                 }
         );
-
-
-
-/*
-        // Temp ===========================================================
-        tempSongs = new ArrayList<>();
-        for (int i = 0; i < 30; ++i) {
-            tempSongs.add("Song number #" + (i + 1));
-        }
-
-        if (mSongsAdapter == null) {
-            mSongsAdapter = new SongsAdapter(tempSongs);
-        }
-
-        //================================================================
-
-*/
-
-
         return view;
     }
 
     private class SongsAdapter extends RecyclerView.Adapter<SongsAdapter.SongHolder> {
 
-        private List<String> mSongsTitles;
+        private List<HashMap<String, String>> mSongsTitles;
 
-        public SongsAdapter(List<String> songs) {
-            mSongsTitles = songs;
+        public SongsAdapter(List<HashMap<String, String>> songs) {
+            this.mSongsTitles = songs;
         }
 
         @NonNull
@@ -148,7 +109,8 @@ public class SongsListFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(@NonNull SongHolder holder, int position) {
-            holder.get_mSongTitle().setText(mSongsTitles.get(position));
+            holder.get_mSongTitle().setText(mSongsTitles.get(position).get("file_name"));
+            holder.set_mSongPath(mSongsTitles.get(position).get("file_path"));
         }
 
         @Override
@@ -159,6 +121,7 @@ public class SongsListFragment extends Fragment {
 
         private class SongHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
             private final TextView mSongTitle;
+            private String mSongPath;
 
             public SongHolder(@NonNull View itemView) {
                 super(itemView);
@@ -170,6 +133,14 @@ public class SongsListFragment extends Fragment {
                 return mSongTitle;
             }
 
+            public String get_mSongPath() {
+                return mSongPath;
+            }
+
+            public void set_mSongPath(String mSongPath) {
+                this.mSongPath = mSongPath;
+            }
+
             @Override
             public void onClick(View view) {
                 Toast.makeText(getContext(), get_mSongTitle().getText().toString() + " Clicked!",
@@ -177,6 +148,11 @@ public class SongsListFragment extends Fragment {
 
                 FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                 Fragment fragment = new SongDetailFragment();
+
+                Bundle bundle = new Bundle();
+                bundle.putString("path", mSongPath);
+                bundle.putString("name", mSongTitle.getText().toString());
+                fragment.setArguments(bundle);
 
                 fragmentManager.beginTransaction()
                         .setReorderingAllowed(true)
@@ -192,7 +168,7 @@ public class SongsListFragment extends Fragment {
 
         try {
             File rootFolder = new File(rootPath);
-            File[] files = rootFolder.listFiles(); //here you will get NPE if directory doesn't contains  any file,handle it like this.
+            File[] files = rootFolder.listFiles();
             for (File file : files) {
                 if (file.isDirectory()) {
                     if (getPlayList(file.getAbsolutePath()) != null) {
@@ -213,57 +189,5 @@ public class SongsListFragment extends Fragment {
             Log.d("scan", e.toString());
         }
         return null;
-    }
-
-    public List<AudioModel> getAllAudioFromDevice(final Context context, String folder) {
-
-        final List<AudioModel> tempAudioList = new ArrayList<>();
-
-        Log.d("scan", "Inside getAudio");
-
-        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        String[] projection = {
-                MediaStore.Audio.AudioColumns.DATA,
-                MediaStore.Audio.AudioColumns.ALBUM,
-                MediaStore.Audio.ArtistColumns.ARTIST,
-        };
-        Cursor c = context.getContentResolver().query(
-                uri,
-                projection,
-                MediaStore.Audio.Media.DATA + " like ? ",
-                new String[]{"%/storage/emulated/0/C_E_M%"},
-                null
-        );
-
-        if (c == null) {
-            Log.d("scan", "Cursor is null");
-        }
-
-        if (c != null) {
-            Log.d("scan", "Cursor not null ");
-            while (c.moveToNext()) {
-                AudioModel audioModel = new AudioModel();
-                String path = c.getString(0);
-                String album = c.getString(1);
-                String artist = c.getString(2);
-
-                String name = path.substring(path.lastIndexOf("/") + 1);
-
-                audioModel.set_mName(name);
-                audioModel.set_mAlbum(album);
-                audioModel.set_mArtist(artist);
-                audioModel.set_mPath(path);
-
-                Log.e("Name :" + name, " Album :" + album);
-                Log.e("Path :" + path, " Artist :" + artist);
-
-                Log.d("scan", "Name " + name);
-
-                tempAudioList.add(audioModel);
-            }
-            c.close();
-        }
-        Log.d("scan", "Scan method EXIT");
-        return tempAudioList;
     }
 }
