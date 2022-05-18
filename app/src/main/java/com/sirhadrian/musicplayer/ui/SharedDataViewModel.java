ab08b7699acbf87f7ed17d9a0a9d5b377ba0862a
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-05-18 21:44:50 +0300

    shared view model for changing data

---
+++ app/src/main/java/com/sirhadrian/musicplayer/ui/SharedDataViewModel.java
@@ -0,0 +1,20 @@
+package com.sirhadrian.musicplayer.ui;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.sirhadrian.musicplayer.model.SongModel;
+
+public class SharedDataViewModel extends ViewModel {
+
+    private final MutableLiveData<SongModel> mPlayingNow = new MutableLiveData<>();
+
+    public void select(SongModel song){
+        mPlayingNow.setValue(song);
+    }
+
+    public LiveData<SongModel> getPlayingNow() {
+        return mPlayingNow;
+    }
+}
